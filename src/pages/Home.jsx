3d4c2b348ff78e386f3a3772cde11cb7ab265967
濵田沙羅 <sarada20020628@gmail.com>
2026-05-06 11:34:25 +0900

    feat: WorksセクションをHome.jsxに追加

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,6 +33,41 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      {/* Worksセクション */}
+      <section id="works" className={styles.works}>
+        <h2 className={styles.topic}>work</h2>
+        <div className={styles.worksList}>
+          <div className={styles.worksContents}>
+            <a href="/work/donut"><img src="/assets/imgs/work-donut.png" alt="カフェどぅなつ" /></a>
+            <p className={styles.contentsTxt}>【仮想サイト】カフェ"どぅなつ"</p>
+          </div>
+          <div className={styles.worksContents}>
+            <a href="/work/sugutabe"><img src="/assets/imgs/work-sugutabe.png" alt="食品宅配サービスLP" /></a>
+            <p className={styles.contentsTxt}>【学校課題】食品宅配サービスLP</p>
+          </div>
+          <div className={styles.worksContents}>
+            <a href="/work/slash"><img src="/assets/imgs/work-slash.png" alt="エステサロンslash" /></a>
+            <p className={styles.contentsTxt}>【仮想サイト】エステサロン"slash"</p>
+          </div>
+          <div className={styles.worksContents}>
+            <a href="/work/esute"><img src="/assets/imgs/work-esute.jpg" alt="フェイシャルエステ" /></a>
+            <p className={styles.contentsTxt}>【架空】フェイシャルエステのキャンペーンバナー</p>
+          </div>
+          <div className={styles.worksContents}>
+            <a href="/work/kissatenn"><img src="/assets/imgs/work-kissatenn.png" alt="旅行記事サイト" /></a>
+            <p className={styles.contentsTxt}>【架空】旅行記事サイトのアイキャッチ</p>
+          </div>
+          <div className={styles.worksContents}>
+            <a href="/work/strawberry"><img src="/assets/imgs/strawberry-pop.png" alt="新商品宣伝バナー" /></a>
+            <p className={styles.contentsTxt}>【架空】新商品の宣伝バナー</p>
+          </div>
+          <div className={styles.worksContents}>
+            <a href="/work/reizouko"><img src="/assets/imgs/reizouko.png" alt="整理整頓POP" /></a>
+            <p className={styles.contentsTxt}>【クライアントワーク】整理整頓POP</p>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
