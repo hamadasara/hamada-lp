4809f1f8e027e807824c7bd33c5e434a1af224ca
濵田沙羅 <sarada20020628@gmail.com>
2026-05-06 11:14:50 +0900

    feat: AboutセクションをHome.jsxに追加

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,26 @@ const Home = () => {
           <img src="/assets/imgs/mainvisual.png" alt="" />
         </p>
       </section>
+      {/* aboutセクション */}
+      <section id="about" className={styles.about}>
+        <p className={styles.aboutLoad}>
+          <img src="/assets/imgs/about-load.png" alt="" />
+        </p>
+        <div className={styles.aboutBox}>
+          <div className={styles.aboutTtl}>
+            <p className={styles.topic}>about</p>
+            <p className={styles.aboutTxt}>
+            クライアントの「こうしたい」という想いを丁寧に引き出し、わかりやすくチームに伝え、<br/>
+              システムとして形にできるエンジニアを目指しています。<br />
+              また、デザインやユーザー体験の視点も取り入れ、「使いやすさ」と「見た目の美しさ」の<br/>
+              どちらも大切にできる存在になりたいと考えています。
+            </p>
+          </div>
+          <div className={styles.aboutMore}>
+            <p><a href="/about">more</a></p>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
